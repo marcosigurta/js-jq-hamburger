e2c2a0580685d981bff8efc7e738aeed77013fb9
marco <marcosigurta.ovb@gmail.com>
2020-07-02 13:56:09 +0200

    Display fixed on Bonus

--- Bonus/main.js
+++ Bonus/main.js
@@ -1,9 +1,13 @@
 
 $('.fa-bars').click(function(){
     $('.hamburger-menu').addClass('active');
+    $('.hidden').removeClass('hidden');
 });
 
 $('.fa-times').click(function(){
     $('.hamburger-menu').removeClass('active');
+    $('.hamburger-menu a').addClass('hidden');
 });
+
+
 
